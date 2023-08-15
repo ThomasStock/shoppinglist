631d22396e792dd090cc5f8baeb1e0ed5a5476d8
Thomas Stock <thomas@stackhouse.be>
2023-08-15 10:18:15 +0200

    contenteditable experiments

--- src/EntriesList.tsx
+++ src/EntriesList.tsx
@@ -1,18 +1,46 @@
+import { useEffect, useRef, useState } from "react";
 import { getCurrentList, useStore } from "./useStore";
+import ContentEditable from "react-contenteditable";
 
 export const EntriesList = () => {
   const { entries } = useStore(getCurrentList);
+  const [text, setText] = useState("");
+  const divref = useRef<HTMLDivElement>(null);
 
-  const hasItems = entries.length;
-  if (!hasItems) {
-    return <div>No items yet</div>;
-  }
+  useEffect(() => {
+    if (!entries.length) {
+      setText("");
+      return;
+    }
+    setText(entries.map((_) => `<p style="display: inline">${_.name}</p>`).join("</br>"));
+  }, [entries]);
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const ps = document.querySelectorAll(".editable p");
+    const rects = [...ps.values()].map((_) => {
+      const rect = _.getBoundingClientRect();
+      return rect;
+    });
+    console.log(JSON.stringify(rects[2], null, 1));
+    const rec = rects[1];
+    const divrect = divref.current!.getBoundingClientRect();
+    const mousex = e.nativeEvent.pageX;
+    const mousey = e.nativeEvent.pageY;
+    console.log(mousex - rec.right, mousey >= rec.top && mousey <= rec.bottom);
+    console.log({ mousex, mousey, r: rec.right, l: rec.y });
+  };
 
   return (
-    <ul>
-      {entries.map(({ id, name }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
+    <ContentEditable
+      innerRef={divref}
+      className="editable"
+      tagName="pre"
+      html={text} // innerHTML of the editable div
+      onClick={(e) => handleClick(e)}
+      onMouseMove={(e) => {
+        //console.log({ clientx: e.clientX, pagex: e.pageX, ox: e.screenX });
+      }}
+      onChange={(e) => setText(e.target.value)} // handle innerHTML change
+    />
   );
 };
